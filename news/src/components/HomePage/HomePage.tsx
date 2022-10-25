dd638573b19df4fc584591040dc888e4c47bac89
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-25 21:07:00 +0200

    code added to HomePage.tsx

--- news/src/components/HomePage/HomePage.tsx
+++ news/src/components/HomePage/HomePage.tsx
@@ -1,41 +1,58 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import {API_KEY} from "../../helpers/helpers"
-
+import { API_KEY } from '../../helpers/helpers';
+import { Typography, List } from '@mui/material';
 
 const HomePage = () => {
-  const [todaysArticles, setTodaysArticles] = useState([]);
+	const [todaysArticles, setTodaysArticles] = useState([]);
 
 	useEffect(() => {
+		const today = new Date();
+		console.log(today);
 
-    const today = new Date();
-    console.log(today);
+		const day =
+			today.getDate() - 1 < 10
+				? `0${today.getDate() - 1}`
+				: today.getDate() - 1;
 
-    const day = today.getDate() - 1 < 10 
-    ? `0${today.getDate() -1}`
-    : today.getDate() -1
+		const month =
+			today.getMonth() + 1 < 10
+				? `0${today.getMonth() + 1}`
+				: today.getMonth() + 1;
 
-    const month = today.getMonth() + 1 < 10
-    ? `0${today.getMonth() +1}`
-    : today.getMonth() +1
+		const year = today.getFullYear();
 
-    const year = today.getFullYear();
+		const date = `${year}-${month}-${day}`;
 
-    const date = `${year}-${month}-${day}`
-   
-		axios.get(
-      `https://newsapi.org/v2/everything?q=world&from=${date}&language=en&sortBy=popularity&apiKey=${API_KEY}`
-    )
-    .then((response) => {
-      // console.log(response.data.articles);
-      setTodaysArticles(response.data.articles);
-    })
-    .catch((err) => {
-      console.error(err.message);
-    });
-  }, []);
+		axios
+			.get(
+				`https://newsapi.org/v2/everything?q=world&from=${date}&language=en&sortBy=popularity&apiKey=${API_KEY}`
+			)
+			.then((response) => {
+				// console.log(response.data.articles);
+				setTodaysArticles(response.data.articles);
+			})
+			.catch((err) => {
+				console.error(err.message);
+			});
+	}, []);
 
-	return <div>HomePage</div>;
+	return (
+		<>
+			<Typography
+				variant="h2"
+				align="center"
+				sx={{ fontSize: '2rem', mb: '0.8rem' }}
+			>
+				Today's hottest news:
+			</Typography>
+			<List sx={{ width: '100%', alignContent: 'center' }}>
+
+      </List>
+		</>
+	);
 };
 
 export default HomePage;
+
+// Your API key is: d2bc634a5b0e46b6823fb6c94a15e10b
