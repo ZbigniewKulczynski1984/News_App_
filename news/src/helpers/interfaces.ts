fbb920f1cab6cabcddec3e68fb29996cf249118a
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-27 19:27:00 +0200

    code added

--- news/src/helpers/interfaces.ts
+++ news/src/helpers/interfaces.ts
@@ -1,11 +1,16 @@
 export interface ArticleObj {
-    url: string;
-    urlToImage: string;
-    title: string;
-  }
-  
-  export interface ArticleProps {
-    art: ArticleObj;
-    key: string;
-  }
-  
+	url: string;
+	urlToImage: string;
+	title: string;
+}
+
+export interface ArticleProps {
+	art: ArticleObj;
+	key: string;
+}
+
+export interface RegisterFormData {
+	email: string;
+	password: string;
+	password2: string;
+}
