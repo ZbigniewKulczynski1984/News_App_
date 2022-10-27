55f47bbbeac7397848e80000fb15754f0afe7436
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-27 19:39:47 +0200

    coder added

--- news/src/App.tsx
+++ news/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<HomePage />} />
+					<Route path="/register" element={<RegisterPage />} />
 				</Routes>
 				<HomePage />
 			</BrowserRouter>
