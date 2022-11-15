d05258c944628116ae376f4c80a46f011a4402d0
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-15 20:34:51 +0100

    code added

---
+++ news/src/components/UserPage/UserPage.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavbarProps as UserPageProps } from "../../helpers/interfaces";
+import { auth } from "../../firebaseConfig";
+import { Typography, Button } from "@mui/material";
+import { signOut } from "firebase/auth";
+const UserPage: React.FC<UserPageProps> = ({ loggedIn }) => {
+  return (
+    <>
+      {loggedIn && auth.currentUser && (
+        <>
+          <Typography
+            variant="h2"
+            align="center"
+            sx={{
+              fontSize: "2rem",
+              my: "1rem",
+              borderBottom: " 1px solid #1976d2",
+              pb: ".5rem",
+            }}
+          >
+            Your profile
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            sx={{ fontSize: "1rem", my: "1rem" }}
+          >
+            {auth.currentUser.email}
+          </Typography>
+          <Button
+            variant="outlined"
+            sx={{ display: "block", mx: "auto", my: "1rem" }}
+            onClick={() => signOut(auth)}
+          >
+            Log out
+          </Button>
+        </>
+      )}
+    </>
+  );
+};
+
+export default UserPage;
