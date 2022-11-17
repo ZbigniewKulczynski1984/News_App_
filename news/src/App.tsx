d3104b06cb990826888c8c61041ed7a355c6de4a
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-17 17:58:21 +0100

    modiefied

--- news/src/App.tsx
+++ news/src/App.tsx
@@ -1,42 +1,36 @@
 import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/HomePage/HomePage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
-import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage/LoginPage";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebaseConfig";
 import UserPage from "./components/UserPage/UserPage";
-
-
-
 function App() {
-	const [loggedIn, setLoggedIn] = useState(false);
-  
-	onAuthStateChanged(auth, (user) => {
-	  if (user) {
-		setLoggedIn(true);
-	  } else {
-		setLoggedIn(false);
-	  }
-	});
+  const [loggedIn, setLoggedIn] = useState(false);
 
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  });
 
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Navbar loggedIn={loggedIn} />
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/register" element={<RegisterPage />} />
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/user" element={<UserPage loggedIn={loggedIn} />} />
-					<Route path="/search" element={<SearchPage />} />
-				</Routes>
-				<HomePage />
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar loggedIn={loggedIn} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/user" element={<UserPage loggedIn={loggedIn} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
